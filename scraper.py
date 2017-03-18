f28a58ee23f7346ebbd2ea56db00729fd9c1d21c
Gautham Goli <goligautham@gmail.com>
2017-03-19 04:18:12 +0530

    Add yahooFinanceNews class and port code to Python3

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
-import urllib
-import urllib2
+from urllib.request import urlopen
+import urllib.request
+import urllib.parse
 import json
+import threading
 import datetime
 import feedparser
+from queue import Queue
 from newspaper import Article
 from bs4 import BeautifulSoup
 from cik import maps
@@ -24,8 +27,8 @@ class GoogleNewsScraper:
         :param search_terms:
         :return:
         """
-        self.web_request = urllib2.Request('http://www.google.com/search?q={query}'.format(query=company_name))
-        self.news_request = urllib2.Request('http://www.google.com/search?q={query}&tbm=nws'.format(query=company_name))
+        self.web_request = urllib.request.Request('https://www.google.com/search?q={query}'.format(query=company_name))
+        self.news_request = urllib.request.Request('https://www.google.com/search?q={query}&tbm=nws'.format(query=company_name))
         self.web_request.add_header('User-Agent', self.user_agent)
         self.news_request.add_header('User-Agent', self.user_agent)
         self.article_objects_parsed = []
@@ -36,7 +39,10 @@ class GoogleNewsScraper:
         for newspaper module to parse them.
         :return:
         """
-        news_response = urllib2.urlopen(self.news_request)
+        try:
+            news_response = urlopen(self.news_request)
+        except:
+            return []
         news_response_html = news_response.read()
         soup = BeautifulSoup(news_response_html, 'html.parser')
         news_soup_body = soup.body
@@ -67,6 +73,44 @@ class GoogleNewsScraper:
 
         return self.article_objects_parsed
 
+class YahooFinanceNewsScaper:
+    def __init__(self):
+        self.finance_url = 'http://finance.yahoo.com/rss/headline?s={company_name}'
+        self.article_objects_parsed = []
+
+    def fetch_news_results(self, company_name):
+        """
+        This method returns the first page search result urls in a dictionary representation
+        for newspaper module to parse them.
+        :return:
+        """
+        self.article_objects_parsed = []
+        feed = feedparser.parse('http://finance.yahoo.com/rss/headline?'+
+                                urllib.parse.urlencode({'s':company_name}))
+
+        news_search_links = [search_result.link
+                             for search_result in feed['entries']]
+        cleaned = {'news-' + str(index): link for index, link in enumerate(news_search_links)}
+        return cleaned
+
+    def parse_news_articles(self, company_name):
+        """
+        Returns newspaper's Article instances of the scraped first page news results
+        :return:
+        """
+        self.article_objects_parsed = []
+        news_urls_dict = self.fetch_news_results(company_name)
+        print(company_name)
+        for id in news_urls_dict:
+            try:
+                url = news_urls_dict[id]
+                article = Article(url)
+                article.download()
+                article.parse()
+                self.article_objects_parsed.append(article)
+            except Exception as e:
+                continue
+        return self.article_objects_parsed
 
 class Dow30Scraper:
     def __init__(self):
@@ -75,7 +119,7 @@ class Dow30Scraper:
         """
         self.dow30url = "http://money.cnn.com/data/dow30/"
         self.user_agent = "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
-        self.dow_request = urllib2.Request(self.dow30url)
+        self.dow_request = urllib.request.Request(self.dow30url)
         self.dow_request.add_header('User-Agent',
                                     self.user_agent)
         self.dow30prices = []
@@ -83,6 +127,7 @@ class Dow30Scraper:
     def scrape_prices(self):
         """
         Returns a dictionary representation of DOW 30 company stock prices and their change
+        For top 5 best and worst performers in ascending order
         {
           "company_name": "name",
           "symbol" : "NN",
@@ -91,7 +136,7 @@ class Dow30Scraper:
         }
         :return:
         """
-        dow_response_html = urllib2.urlopen(self.dow_request).read()
+        dow_response_html = urlopen(self.dow_request).read()
         soup = BeautifulSoup(dow_response_html, 'html.parser')
         dow_soup_body = soup.body
         dow_table = dow_soup_body.find("table", "wsod_dataTable")
@@ -102,11 +147,16 @@ class Dow30Scraper:
             price_cols = row.find_all("span", "wsod_stream")
             current_price = float(price_cols[0].text)
             change_in_price = float(price_cols[1].find("span").text)
+            price_change_percentage = float(price_cols[2].find("span").text.strip('%'))
             self.dow30prices.append({"company_name": name,
                                      "symbol": symbol,
                                      "current_price": current_price,
-                                     "change": change_in_price})
-        return self.dow30prices
+                                     "change": change_in_price,
+                                     "change_percentage": price_change_percentage})
+
+        # Sort the companies according to change %
+        self.dow30prices = sorted(self.dow30prices, key=lambda data: data['change_percentage'])
+        return self.dow30prices[:5] + self.dow30prices[-5:]
 
 
 class K8Scraper:
@@ -127,7 +177,7 @@ class K8Scraper:
                        'count': 30,
                        'output': 'atom'}
         self.user_agent = "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
-        self.dow_request = urllib2.Request(self.k8url)
+        self.dow_request = urllib.request.Request(self.k8url)
         self.dow_request.add_header('User-Agent',
                                     self.user_agent)
         self.k8_filings_parsed = []
@@ -144,7 +194,7 @@ class K8Scraper:
         cik_number = maps[company_symbol]
         self.params['CIK'] = cik_number
 
-        rss_request_url = self.k8url + '?' + urllib.urlencode(self.params)
+        rss_request_url = self.k8url + '?' + urllib.parse.urlencode(self.params)
         feed = feedparser.parse(rss_request_url)
         for entry in feed['entries']:
             filing_date = datetime.datetime.strptime(entry['filing-date'], "%Y-%m-%d").date()
@@ -159,7 +209,7 @@ class K8Scraper:
         :param url:
         :return:
         """
-        response_html = urllib2.urlopen(url).read()
+        response_html = urlopen(url).read()
         soup = BeautifulSoup(response_html, 'html.parser')
         k8_filings_body = soup.body
         k8_filing_link = k8_filings_body.find("table", "tableFile").find("a").get("href")
@@ -173,10 +223,14 @@ class K8Scraper:
         """
         self.k8_filings_parsed = []
         for k8_url in self.k8_urls:
-            article = Article(k8_url)
-            article.download()
-            article.parse()
-            self.k8_filings_parsed.append(article)
+            try:
+                article = Article(k8_url)
+                article.download()
+                article.parse()
+                self.k8_filings_parsed.append(article)
+            except Exception as e:
+                print(e.message)
+                continue
         return self.k8_filings_parsed
 
 
@@ -201,25 +255,48 @@ class ArticleAnalyser:
 
         return [article, senti_score]
 
+def download_and_parse(article_url, yahoo):
+    try:
+        article = Article(article_url)
+        article.download()
+        article.parse()
+        yahoo.article_objects_parsed.append(article)
+    except Exception as e:
+        pass
+
+def worker():
+    while True:
+        item = q.get()
+        url, yahooNews = item
+        download_and_parse(url, yahooNews)
+        q.task_done()
+
 if __name__ == '__main__':
 
     # Examples
     # Scrape current DOW 30 prices
     dowToday = Dow30Scraper()
-    dowToday.scrape_prices()
+    companies = dowToday.scrape_prices()
 
     # Scrape and parse news related to company
-    googleNews = GoogleNewsScraper()
-    googleNews.query('3M')
-    article_objects_news = googleNews.parse_news_articles()
-
-
-    k8scraper = K8Scraper()
-    # Fetch recent k8 filings for a Company
-    k8scraper.fetch_recent_k8_filings('MMM')
-    article_objects_k8_axp = k8scraper.parse_k8_filings()
-
+    yahooNews = YahooFinanceNewsScaper()
     analyser = ArticleAnalyser()
+    for company in companies:
+        article_urls = yahooNews.fetch_news_results(company['symbol']).values()
+        q = Queue()
+        for i in range(4):
+            t = threading.Thread(target=worker)
+            t.daemon = True
+            t.start()
+        for url in article_urls:
+            q.put([url, yahooNews])
 
-    for article_obj in article_objects_news:
-        print analyser.analyse(article_obj)
+        q.join()
+        print(company['company_name'], ': Change % :', company['change_percentage'])
+        for article_obj in yahooNews.article_objects_parsed:
+            print(analyser.analyse(article_obj))
+
+    # k8scraper = K8Scraper()
+    # # Fetch recent k8 filings for a Company
+    # k8scraper.fetch_recent_k8_filings('UNH')
+    # article_objects_k8_axp = k8scraper.parse_k8_filings()
